993f4fd33e722631339f062e97a4683ab71f5f62
Robert Walsh <rwalsh@excalibur-solutions.com>
2024-10-07 11:58:47 -0400

    Add script.js and import it into the HTML

---
+++ script.js
@@ -0,0 +1,12 @@
+/*
+ *  Weather Report
+ *
+ *  This program uses the OpenWeatherMap API to retrieve
+ *  and display current weather information for a city/state
+ *  or a ZIP code.
+ * 
+ *  Author: Robert Walsh
+ *  Date:   October 7, 2024
+ * 
+ */
+
